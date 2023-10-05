dbb01f13f86463ea7e229b8ec4db91053dc97971
kiriti07 <ch.kiriti@gmail.com>
2023-10-05 07:27:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,48 +1,19 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
 }
 
-variable "vpc_name" {
-  description = "Name of the VPC"
-  default     = "Development VPC"
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
 }
 
-variable "public_subnet_1_cidr" {
-  description = "CIDR block for Public Subnet 1"
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
 }
 
-variable "public_subnet_2_cidr" {
-  description = "CIDR block for Public Subnet 2"
-}
-
-variable "private_subnet_1_cidr" {
-  description = "CIDR block for Private Subnet 1"
-}
-
-variable "private_subnet_2_cidr" {
-  description = "CIDR block for Private Subnet 2"
-}
-
-variable "private_subnet_3_cidr" {
-  description = "CIDR block for Private Subnet 1"
-}
-
-variable "private_subnet_4_cidr" {
-  description = "CIDR block for Private Subnet 2"
-}
-
-variable "public_subnet_1_az" {
-  description = "Availability Zone for Public Subnet 1"
-}
-
-variable "public_subnet_2_az" {
-  description = "Availability Zone for Public Subnet 2"
-}
-
-variable "private_subnet_1_az" {
-  description = "Availability Zone for Private Subnet 1"
-}
-
-variable "private_subnet_2_az" {
-  description = "Availability Zone for Private Subnet 2"
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
 }

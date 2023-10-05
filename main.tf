7ae5aa9e556e88b2fba98d3c0cd9de3b78261380
kiriti07 <ch.kiriti@gmail.com>
2023-10-05 07:27:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,7 @@
-module "vpc" {
-  source  = "./modules/vpc"
-  vpc_cidr = var.vpc_cidr
-  vpc_name = var.vpc_name
-  public_subnet_1_cidr   = var.public_subnet_1_cidr
-  public_subnet_2_cidr   = var.public_subnet_2_cidr
-  private_subnet_1_cidr  = var.private_subnet_1_cidr
-  private_subnet_2_cidr  = var.private_subnet_2_cidr
-  private_subnet_3_cidr  = var.private_subnet_3_cidr
-  private_subnet_4_cidr  = var.private_subnet_4_cidr
-  public_subnet_1_az     = var.public_subnet_1_az
-  public_subnet_2_az     = var.public_subnet_2_az
-  private_subnet_1_az    = var.private_subnet_1_az
-  private_subnet_2_az    = var.private_subnet_2_az
+module "ec2" {
+  source       = "./modules/ec2"
+  instance_type = var.instance_type
+  ami           = var.ami
+  vpc_id        = var.vpc_id
+  subnet_id     = var.subnet_id
 }
